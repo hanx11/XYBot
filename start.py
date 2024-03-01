3503042e4b430b3d600b6398b10d12cc6887d626
hanzq <zhiqun.han@hannto.com>
2024-03-01 14:14:41 +0800

    update start.py

--- start.py
+++ start.py
@@ -65,17 +65,15 @@ async def create_chat_gpt_dialog(message):
                 pk = r_json['result']['pk']
                 url = f"https://sg-api-ai.jiyinglobal.com/v1/m/gpt/chat/5445436cd51e11eeb8be4f8b59949224/completion/?pk={pk}"
                 request = requests.Request(method='GET', url=url, headers=headers).prepare()
-                r = session.send(request, timeout=2).json()
-                logger.info(f"{r}")
                 content = ""
                 while True:
+                    r = session.send(request, timeout=2).json()
+                    logger.info(f"{r}")
                     if r.get("code") != "0" or (r['result']['status'] == 0 and not r['result']['content']):
                         break
 
                     content += r['result']['content']
                     await asyncio.sleep(r['result']['wait'] / 1000)
-                    r = session.send(request, timeout=2).json()
-                    logger.info(f"{r}")
                 return content
 
         return "服务器开小差了，请稍后再试^_^"
